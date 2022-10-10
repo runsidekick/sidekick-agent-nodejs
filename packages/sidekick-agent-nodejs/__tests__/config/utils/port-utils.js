74fe9fa08bc67c0015e269c97a259f46d6a1674d
bcaglayan <batuhan@thundra.io>
2022-10-10 17:26:27 +0300

    Revert "Revert "Added logpoint fields to application status event""
    This reverts commit bcd04fef68a2ca9b484a896f84d9f4dbb920ffcc.

--- packages/sidekick-agent-nodejs/__tests__/config/utils/port-utils.js
+++ packages/sidekick-agent-nodejs/__tests__/config/utils/port-utils.js
@@ -4,8 +4,8 @@ const maximum = 100;
 const minimum = 10;
 
 const getRandomPort = async () => {
-    const randomNumber = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
     const port = await portfinder.getPortPromise();
+    const randomNumber = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 
     return port + randomNumber;
 }
